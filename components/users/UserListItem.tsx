0950675652e02611f628dd94d12a3a1d359376e2
bado <badreddine@pembepanjur.com>
2022-08-07 02:57:58 +0300

    - Likes list => from api

--- components/users/UserListItem.tsx
+++ components/users/UserListItem.tsx
@@ -1,6 +1,6 @@
 import {Image, Text, View} from 'react-native';
 import React from 'react';
-import styles from '../testScreens/styles';
+import styles from './styles';
 
 // @ts-ignore
 const UserListItem = ({user}) => {
@@ -9,8 +9,8 @@ const UserListItem = ({user}) => {
       <View style={styles.pictureContainer}>
         <Image
           source={{
-            // uri: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
-            uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
+            uri: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+            // uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
           }}
           style={[styles.picture]}
         />
@@ -19,9 +19,9 @@ const UserListItem = ({user}) => {
         {/*<Text>{user.id}</Text>*/}
         {/*<Text>{user.name}</Text>*/}
         <Text style={styles.primaryText}>{user.username}</Text>
-        <Text style={styles.secondaryText} >{user.email}</Text>
-        <Text style={styles.secondaryText} >{user.phone}</Text>
-        <Text style={styles.secondaryText} >{user.website}</Text>
+        <Text style={styles.secondaryText}>{user.email}</Text>
+        <Text style={styles.secondaryText}>{user.phone}</Text>
+        <Text style={styles.secondaryText}>{user.website}</Text>
       </View>
     </View>
   );
---
+++ components/users/styles.ts
@@ -0,0 +1,58 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  listItemContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+    width: '100%',
+  },
+  pictureContainer: {
+    width: '20%',
+    height: 100,
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    marginHorizontal: 12,
+  },
+  picture: {
+    width: 70,
+    height: 70,
+    backgroundColor: '#FE474C',
+    borderRadius: 100,
+    shadowColor: 'rgba(0,0,0,0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+  },
+
+  textContainer: {
+    borderBottomWidth: 1,
+    borderColor: '#e0e0e0',
+    height: 100,
+    width: '70%',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    marginHorizontal: 12,
+  },
+
+  primaryText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#000000',
+    textTransform: 'capitalize',
+  },
+  secondaryText: {
+    fontSize: 12,
+    fontWeight: 'normal',
+    color: '#6f6f6f',
+    marginVertical: 1,
+  },
+
+});
+
+export default styles;
